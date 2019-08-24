d1c515847f8fa1e7de519b21847a30b9e456c1d5
MrAnyx <1ptitigre@gmail.com>
2019-08-24 21:29:20 +0200

    [add] value in bdd

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -14,9 +14,11 @@
 		<?= $content ?>
 	</div>
 
-	<footer class="bg-light py-4 footer mt-auto">
+	<footer class="bg-secondary text-light py-4 footer mt-auto">
 		<div class="container">
+			<?php if(defined("DEBUG_TIME")): ?>
 			Page générée en <?= round(1000 * (microtime(true) - DEBUG_TIME)) ?> ms
+			<?php endif ?>
 		</div>
 	</footer>
 
